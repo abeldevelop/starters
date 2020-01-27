b4cf65e5a6ad07431aa1c1ebac73db8fb6eabe41
Abel Fernandez <abelfergodevelop@gmail.com>
2020-01-27 19:59:20 +0100

    Add constant for text of status codes

---
+++ api-starter/src/main/java/com/abeldevelop/architecture/starter/api/SpringFoxConstants.java
@@ -0,0 +1,15 @@
+package com.abeldevelop.architecture.starter.api;
+
+public class SpringFoxConstants {
+
+    private SpringFoxConstants() {
+        
+    }
+    
+    public static final String API_RESPONSE_CODE_200_MESSAGE = "Successful operation";
+    public static final String API_RESPONSE_CODE_201_MESSAGE = "Resource created";
+    public static final String API_RESPONSE_CODE_204_MESSAGE = "Successful operation without content";
+    public static final String API_RESPONSE_CODE_400_MESSAGE = "Error in Request";
+    public static final String API_RESPONSE_CODE_404_MESSAGE = "Resource Not Found";
+    public static final String API_RESPONSE_CODE_500_MESSAGE = "Internal Server Error";
+}
